cc56a05bcb0b3ac6b2a06c2416110abea1225dc9
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-12-03 17:30:42 +0000

    Bumped client version for pip

--- client/setup.py
+++ client/setup.py
@@ -32,7 +32,7 @@ else:
 
 # pylint: disable=W0142
 setup(name='crowdbox',
-      version='0.0.1',
+      version='0.0.2',
       license=APACHE_LICENSE,
       description='Command-line Client for Crowdbox',
       author='Tom Buckley-Houston',
